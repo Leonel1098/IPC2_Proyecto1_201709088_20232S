b53c01f9e17742b11cc70d1fa591165338319e0f
Leonel1098 <gonzalezleonel1098@gmail.com>
2023-08-12 23:09:23 -0600

    Inicio

--- app.py
+++ app.py
@@ -2,24 +2,25 @@ def mostrar_menu():
     print("")
     print("")
     print("-------Menú-----------")
-    print("1. Registrar Paciente")
-    print("2. Registrar Doctor")
-    print("3. Ver pacientes en el sistema")
-    print("4. Ver doctores en el sistema")
-    print("5. Agendar Cita")
-    print("6. salir")
+    print("1. Cargar Archivo")
+    print("2. Procesar Archivo")
+    print("3. Escribir Archivo de Salida")
+    print("4. Mostrar datos del Estudiante")
+    print("5. Generar Gráfica")
+    print("6. Inicializar sistema")
+    print("7. Salida")
     opcion=input("Ingrese una opción del menú: ")
     while True:
         if opcion=="1":
-            registrar_paciente()
+            
         elif opcion=="2":
-            registrar_doctor()
+           
         elif opcion=="3":
-            ver_los_pacientes()
+            
         elif opcion=="4":
-            ver_los_doctores()
+            
         elif opcion=="5":
-            agendar_una_cita()
+            
         elif opcion=="6":
             print("Saliendo del programa, vuevla pronto")
             break
